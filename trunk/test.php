6226c51db095710b0255730f8551c26c1c96ad9a
epithet <epithet@8a82c45c-ea4d-11de-83d8-6d5adf01d787>
2009-12-16 14:18:18 +0000

    Initial commit.

---
+++ trunk/test.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+  <head>
+  <title>Calc Test</title>
+  </head>
+  <body>
+
+<form>
+<input type="text" name="expression" value="<?php echo htmlspecialchars($_GET['expression']); ?>">
+<input type="submit" value="Calc">
+</form>
+<style type="text/css">
+s {
+	color: #999;
+}
+</style>
+
+<pre><?php
+
+include 'calc.php';
+
+$expression = $_GET['expression'];
+
+$calc = new Calc($expression);
+
+echo htmlspecialchars($expression) . "\n";
+echo htmlspecialchars($calc->infix() . " = " . $calc->calc()) . "\n";
+
+?>
+</pre>
+
+  </body>
+</html>
